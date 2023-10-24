985fd41d848064bb3a563357ce1b06878fe48d80
Yeheskiel Andrazein <eki_yeheskiel@hotmail.co.id>
2023-10-24 17:04:20 +0700

    remove unused route

--- src/router/MainRoutes.ts
+++ src/router/MainRoutes.ts
@@ -12,16 +12,6 @@ const MainRoutes = {
             component: () => import('@/views/dashboard/index.vue'),
         },
         {
-            name: 'Typography',
-            path: '/ui/typography',
-            component: () => import('@/views/components/Typography.vue')
-        },
-        {
-            name: 'Shadow',
-            path: '/ui/shadow',
-            component: () => import('@/views/components/Shadow.vue')
-        },
-        {
             name: 'Icons',
             path: '/icons',
             component: () => import('@/views/pages/Icons.vue')
